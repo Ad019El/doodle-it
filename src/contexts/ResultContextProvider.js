5a892a68b294d6b0cfc67de4bd7bdc022c05c1d6
ad019el <a.dafi@esi-sba.dz>
2022-04-04 15:10:48 +0100

    fixed first commit

---
+++ src/contexts/ResultContextProvider.js
@@ -0,0 +1,39 @@
+import { type } from "@testing-library/user-event/dist/type";
+import React, { createContext, useContext, useState } from "react";
+
+const ResultContext = createContext();
+const baseUrl = "https://google-search3.p.rapidapi.com/api/v1";
+
+export const ResultContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: "GET",
+      headers: {
+        "X-User-Agent": "desktop",
+        "X-Proxy-Location": "EU",
+        "X-RapidAPI-Host": "google-search3.p.rapidapi.com",
+        "X-RapidAPI-Key": process.env.REACT_API_KEY,
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+  return (
+    <ResultContext.Provider
+      value={{ getResults, results, searchTerm, setSearchTerm, loading }}
+    >
+      {children}
+    </ResultContext.Provider>
+  );
+};
+
+export const useResultContext = () => useContext(ResultContext);
